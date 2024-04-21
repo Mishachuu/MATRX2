ae01653a74bb6da70529451ec58099c5a8767010
Миша Чукарев <mishakrut11@gmail.com>
2024-04-21 17:56:41 +0400

    fix

--- MATRX2/Source.cpp
+++ MATRX2/Source.cpp
@@ -1,20 +1,22 @@
-#include <iostream>
+п»ї#include <iostream>
 #include <fstream>
 #include <vector>
 #include <chrono>
 #include <iomanip>
 #include <string>
-#include <omp.h> // Подключаем библиотеку OpenMP
+#include <omp.h> 
 
 using namespace std;
 
 void multiply_matrices(const vector<vector<int>>& A, const vector<vector<int>>& B, vector<vector<int>>& C)
 {
-#pragma omp parallel for // Директива OpenMP для параллельного выполнения цикла
-    for (int i = 0; i < C.size(); i++) {
-        for (int j = 0; j < C[i].size(); j++) {
+    omp_set_num_threads(16);
+    int i, j, k;
+#pragma omp parallel for shared(A, B, C) private(i,j,k)
+    for (i = 0; i < C.size(); i++) {
+        for (j = 0; j < C[i].size(); j++) {
             C[i][j] = 0;
-            for (int k = 0; k < C.size(); k++) {
+            for (k = 0; k < C.size(); k++) {
                 C[i][j] += B[i][k] * A[k][j];
             }
         }
@@ -56,6 +58,11 @@ void resize_matrix(vector<vector<int>>& A, int size)
         j.resize(size);
 }
 
+void write_result(string str, int size, double duration, string arr) {
+    ofstream result("result_16.txt", ios::app);
+    result << "Size: " << size << " Time: " << duration << " Г±ГҐГЄГіГ­Г¤" << endl << arr << endl;
+}
+
 int main()
 {
     srand(time(NULL));
@@ -79,17 +86,25 @@ int main()
         filename2 = filename[1];
         read_data(A, filename1.append(to_string(size[i])).append(".txt"));
         read_data(B, filename2.append(to_string(size[i])).append(".txt"));
+        double result = 0;
+        string arr;
 
-        chrono::steady_clock::time_point begin = chrono::steady_clock::now();
-        multiply_matrices(A, B, C);
-        chrono::steady_clock::time_point end = chrono::steady_clock::now();
+        for (int j = 0; j < 10; j++) {
+            chrono::steady_clock::time_point begin = chrono::steady_clock::now();
+            multiply_matrices(A, B, C);
+            chrono::steady_clock::time_point end = chrono::steady_clock::now();
+            double duration = chrono::duration_cast<std::chrono::milliseconds>(end - begin).count();
+            result += duration;
+            arr.append(to_string(duration) + "  ");
+        }
 
         res_filename = filename[2];
         write_result_matrix(res_filename.append(to_string(size[i])).append(".txt"), C);
-
+        result /= 10;
         res.append("For matrix with size of ").append(to_string(size[i])).append("x").append(to_string(size[i]));
-        res.append(" time of multiplication = ").append(to_string(chrono::duration_cast<std::chrono::milliseconds>(end - begin).count())).append(" ms\n\n");
-        cout << "Diff(ms) = " << chrono::duration_cast<std::chrono::milliseconds>(end - begin).count() << endl;
+        res.append(" time of multiplication = ").append(to_string(result)).append(" ms\n\n");
+        cout << "Diff(ms) = " << result << endl;
+        write_result("Matrix_res.txt", size[i], result, arr);
     }
     ofstream data("C:\\Users\\misha\\source\\repos\\MATRX2\\MATRX2\\res.txt");
     data << res;
